166f9bef882d9c228b1684312025e1aba10c00b6
Tanner C <tannerc.bootcamp@gmail.com>
2021-06-02 04:34:45 -0700

    ATTOM API added successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,33 @@ var input = document.getElementById("searchCityInput");
 searchButton.onclick = (event) => {
   var searchInput = input.value;
   fetch(
-    "https://app.ticketmaster.com/discovery/v2/events.json?city=" +
-      searchInput +
-      "&apikey=SKeYU5QgdjyEFDczzoAYWmzg3UKfZ0bh",
+    "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=" +
+      searchInput,
     {
       method: "GET",
+      headers: {
+        apikey: "e8c90852cbe636f996963a3efd80ae52",
+      },
     }
-  )
-    .then(function (response) {
-      if (response.ok) {
-      }
+  ).then(async (response) => {
+    if (response.ok) {
+      var property = await response.json();
       console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      var pic = $(property[0].address.oneLine);
+    }
+    fetch(
+      "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
+        pic +
+        "&key=AIzaSyC8HIJdWQvEw0hgqmcQwWqT4vKnuKgchfw",
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
 };
