3306edbf4aa0d6ef95a9e71d14b565797f5e91c9
Tanner C <tannerc.bootcamp@gmail.com>
2021-06-02 03:36:19 -0700

    implemented response from api server using fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-var description = document.querySelector("#fillDescription");
+var searchButton = document.getElementById("submitBtn");
+var input = document.getElementById("searchCityInput");
+searchButton.onclick = (event) => {
+  var searchInput = input.value;
+  fetch(
+    "https://app.ticketmaster.com/discovery/v2/events.json?city=" +
+      searchInput +
+      "&apikey=SKeYU5QgdjyEFDczzoAYWmzg3UKfZ0bh",
+    {
+      method: "GET",
+    }
+  )
+    .then(function (response) {
+      if (response.ok) {
+      }
+      console.log(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
