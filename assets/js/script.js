93674d22734efeee2143cb3e7f9f1af86ef681d1
Tanner C <tannerc.bootcamp@gmail.com>
2021-06-08 19:19:38 -0700

    Added modal on error/ incorrect zip code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,93 +5,105 @@ function searchCity(cityname) {
   var propertyAPI =
     "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=" +
     cityname;
-  //pull data from first api
+
   $.ajax({
     url: propertyAPI,
     method: "GET",
     headers: {
       apikey: "f03cdca50a0e28cbedc129b325aedc6f",
     },
-  }).then(function (response) {
-    console.log(response);
-    console.log(propertyAPI);
-
-    var searchDiv = document.querySelector("#fillAddy");
-    var pElem = document.createElement("p");
-    var foundAddress = "Address: " + response.property[i].address.oneLine;
-    pElem.className = "styleforP";
-    pElem.innerHTML = foundAddress;
-    searchDiv.appendChild(pElem);
-    //Pull address from first api, plug into second api url
-    var addressPart1 = response.property[i].address.line1;
-    var addressPart2 = response.property[i].address.line2;
-    var propertyDetails =
-      "https://api.gateway.attomdata.com/propertyapi/v1.0.0/sale/detail?address1=" +
-      addressPart1 +
-      "&address2=" +
-      addressPart2;
-    $.ajax({
-      url: propertyDetails,
-      method: "GET",
-      headers: {
-        apikey: "f03cdca50a0e28cbedc129b325aedc6f",
-      },
-    }).then(function (response) {
+  })
+    .then(function (response) {
       console.log(response);
-      console.log(propertyDetails);
-      //Estimated sales price
+      console.log(propertyAPI);
+
       var searchDiv = document.querySelector("#fillAddy");
       var pElem = document.createElement("p");
-      var estimatedSale =
-        "Estimated Sales Price: " + response.property[0].sale.amount.saleamt;
-      pElem.className = "salesPrice";
-      pElem.innerHTML = estimatedSale;
-      searchDiv.appendChild(pElem);
-      //Bedrooms
-      var searchDiv = document.querySelector("#fillAddy");
-      var pElem = document.createElement("p");
-      var bedrooms =
-        "Amount of Bedrooms: " + response.property[0].building.rooms.beds;
-      pElem.className = "bedrooms";
-      pElem.innerHTML = bedrooms;
-      searchDiv.appendChild(pElem);
-      //Bathrooms
-      var searchDiv = document.querySelector("#fillAddy");
-      var pElem = document.createElement("p");
-      var bathrooms =
-        "Amount of Bathrooms: " +
-        response.property[0].building.rooms.bathstotal;
-      pElem.className = "bathrooms";
-      pElem.innerHTML = bathrooms;
+      var foundAddress = "Address: " + response.property[i].address.oneLine;
+      pElem.className = "styleforP";
+      pElem.innerHTML = foundAddress;
       searchDiv.appendChild(pElem);
 
-      //Pull data from first api, plug into 3rd api to pull image
-      var lon = response.property[0].location.latitude;
-      var lat = response.property[0].location.longitude;
-
-      var walkAPI =
-        "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
-        lon +
-        "," +
-        lat +
-        "&key=AIzaSyC8HIJdWQvEw0hgqmcQwWqT4vKnuKgchfw";
-
+      //Pull address from first api, plug into second api url
+      var addressPart1 = response.property[i].address.line1;
+      var addressPart2 = response.property[i].address.line2;
+      var propertyDetails =
+        "https://api.gateway.attomdata.com/propertyapi/v1.0.0/sale/detail?address1=" +
+        addressPart1 +
+        "&address2=" +
+        addressPart2;
       $.ajax({
-        url: walkAPI,
+        url: propertyDetails,
         method: "GET",
-        crossOrigin: "null",
-        mode: "no-cors",
+        headers: {
+          apikey: "f03cdca50a0e28cbedc129b325aedc6f",
+        },
       }).then(function (response) {
-        console.log(walkAPI);
-        //append image
-        var imgSearch = document.querySelector("#fillImg");
-        var imgElem = document.createElement("img");
-        imgElem.className = "searchedImg";
-        imgElem.src = walkAPI;
-        imgSearch.appendChild(imgElem);
+        console.log(response);
+        console.log(propertyDetails);
+
+        //Estimated sales price
+        var searchDiv = document.querySelector("#fillAddy");
+        var pElem = document.createElement("p");
+        var estimatedSale =
+          "Estimated Sales Price: " + response.property[0].sale.amount.saleamt;
+        pElem.className = "salesPrice";
+        pElem.innerHTML = estimatedSale;
+        searchDiv.appendChild(pElem);
+        //Bedrooms
+        var searchDiv = document.querySelector("#fillAddy");
+        var pElem = document.createElement("p");
+        var bedrooms =
+          "Amount of Bedrooms: " + response.property[0].building.rooms.beds;
+        pElem.className = "bedrooms";
+        pElem.innerHTML = bedrooms;
+        searchDiv.appendChild(pElem);
+        //Bathrooms
+        var searchDiv = document.querySelector("#fillAddy");
+        var pElem = document.createElement("p");
+        var bathrooms =
+          "Amount of Bathrooms: " +
+          response.property[0].building.rooms.bathstotal;
+        pElem.className = "bathrooms";
+        pElem.innerHTML = bathrooms;
+        searchDiv.appendChild(pElem);
+
+        //Pull data from first api, plug into 3rd api to pull image
+        var lon = response.property[0].location.latitude;
+        var lat = response.property[0].location.longitude;
+
+        var walkAPI =
+          "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
+          lon +
+          "," +
+          lat +
+          "&key=AIzaSyC8HIJdWQvEw0hgqmcQwWqT4vKnuKgchfw";
+
+        $.ajax({
+          url: walkAPI,
+          method: "GET",
+          crossOrigin: "null",
+          mode: "no-cors",
+        }).then(function (response) {
+          console.log(walkAPI);
+          //append image
+          var imgSearch = document.querySelector("#fillImg");
+          var imgElem = document.createElement("img");
+          imgElem.className = "searchedImg";
+          imgElem.src = walkAPI;
+          imgSearch.appendChild(imgElem);
+        });
+      });
+    })
+    .catch((err) => {
+      document.getElementById("alertModal").className =
+        "modal fixed inline-block rounded-lg overflow-hidden shadow-xl w-full h-full top-0 left-0 flex items-center justify-center";
+      $("#closeBtn").on("click", function (err) {
+        document.getElementById("alertModal").className =
+          "modal fixed w-full h-full top-0 left-0 flex items-center justify-center opacity-0 pointer-events-none";
       });
     });
-  });
+
   //clear apis data
   $("#fillAddy").empty();
   $("#fillImg").empty();
